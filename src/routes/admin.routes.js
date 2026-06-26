497fcc6cbf2bfe86ee219f00af03f37d64dbc4e0
Ansh vishwakarma <vishwaansh244@gmail.com>
2026-06-26 12:07:27 +0530

    Add UpdatePassword route to admin routes for password management

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,10 @@
 import { Router} from "express";
-import { SetPassword } from "../controllers/adminController.js";
+import { SetPassword, UpdatePassword } from "../controllers/adminController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const adminRoutes = new Router();
 
 adminRoutes.post("/setpassword",verifyToken,  SetPassword) ;
+adminRoutes.put("/updatepassword", verifyToken, UpdatePassword);
 
 export default adminRoutes;
